15874851e0d9367ad73489febdbf592708fbbd6d
Vidit Shah <vidit0210@protonmail.com>
2024-02-06 15:38:37 +0530

    updated API Gateway

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-"use client";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
-import { useUserStore } from "@/store/userStore";
-import { useEffect } from "react";
 import ConfigureAmplifyClientSide from "@/ConfigureAmplifyClientSide";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,11 +12,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { fetchUsername } = useUserStore();
-
-  useEffect(() => {
-    fetchUsername();
-  }, []);
   return (
     <html lang="en">
       <body className={inter.className}>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 import NavBar from "@/components/NavBar";
 import Todos from "@/components/Todos";
-import Greet from "@/components/greet";
 
+import { useUserStore } from "@/store/userStore";
+import { useEffect } from "react";
 function Page() {
+  const { fetchUsername } = useUserStore();
+
+  useEffect(() => {
+    fetchUsername();
+  }, []);
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-8">
       <NavBar />
